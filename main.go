5962edd2000792875632bf7ce8375fb100d4431f
Nicolas <n.riquelmebareiro@gmail.com>
2022-10-19 18:44:44 +0200

    Allow credentials

--- main.go
+++ main.go
@@ -39,9 +39,12 @@ func (ht *HeaderTransform) ServeHTTP(rw http.ResponseWriter, req *http.Request) 
 		}
 	}
 
+	if rw.Header().Get("Access-Control-Allow-Credentials") == "" {
+		rw.Header().Set("Access-Control-Allow-Credentials", "true")
+	}
+
 	rw.Header().Set("Origin", req.Header.Get("Origin"))
 	rw.Header().Set("Access-Control-Allow-Origin", req.Header.Get("Origin"))
-	rw.Header().Set("Access-Control-Allow-Credentials", "true")
 	rw.Header().Set("Vary", "Origin")
 
 	log.Print(rw.Header());
