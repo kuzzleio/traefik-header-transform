159af158bce63bd77ce4b6b054f3454b1a5d5817
Nicolas <n.riquelmebareiro@gmail.com>
2022-10-20 16:15:46 +0200

    Other changes

--- main.go
+++ main.go
@@ -33,14 +33,14 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 func (ht *HeaderTransform) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
 	if req.Header.Get("Origin") == "null" {
 		if req.Header.Get("Referrer") != "" {
-			req.Header.Set("Origin", req.Header.Get("Referrer"))
+			rw.Header().Add("Origin", req.Header.Get("Referrer"))
 		} else {
-			req.Header.Set("Origin", req.Header.Get("Referer"))
+			rw.Header().Add("Origin", req.Header.Get("Referer"))
 		}
 
 	}
 	
-	rw.Header().Add("Access-Control-Allow-Origin", "")
+	rw.Header().Del("Access-Control-Allow-Origin")
 	rw.Header().Add("Access-Control-Allow-Credentials", "true")
 	rw.Header().Add("Vary", "Origin")
 
