7f15984c312601e052750e1d1d7add4d2ac64b48
Nicolas <n.riquelmebareiro@gmail.com>
2022-10-20 16:52:08 +0200

    simple test

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package traefik_header_transform
 
 import (
 	"context"
-	"log"
 	"net/http"
 )
 
@@ -31,21 +30,9 @@ func New(ctx context.Context, next http.Handler, config *Config, name string) (h
 }
 
 func (ht *HeaderTransform) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
-	if req.Header.Get("Origin") == "null" {
-		if req.Header.Get("Referrer") != "" {
-			rw.Header().Add("Origin", req.Header.Get("Referrer"))
-		} else {
-			rw.Header().Add("Origin", req.Header.Get("Referer"))
-		}
-		req.Header.Del("Origin");
-	}
-	
-	rw.Header().Add("Access-Control-Allow-Origin", rw.Header().Get("Origin"))
+	rw.Header().Add("Access-Control-Allow-Origin", req.Header.Get("Origin"))
 	rw.Header().Add("Access-Control-Allow-Credentials", "true")
 	rw.Header().Add("Vary", "Origin")
-
-	log.Print("Response", rw.Header())
-	log.Print("Request", req.Header)
 
 	ht.next.ServeHTTP(rw, req)
 }
