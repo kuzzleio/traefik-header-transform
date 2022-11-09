6b60472b73d395b237726fab4d4339d381fadae6
Nicolas <n.riquelmebareiro@gmail.com>
2022-11-09 07:21:10 +0100

    Fixing issue with double headers

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package traefik_header_transform
 
 import (
 	"context"
+	"log"
 	"net/http"
 )
 
@@ -10,9 +11,9 @@ type Config struct {
 }
 
 type HeaderTransform struct {
-	next   http.Handler
-	headers  map[string]string
-	name   string
+	next    http.Handler
+	headers map[string]string
+	name    string
 }
 
 func CreateConfig() *Config {
@@ -22,20 +23,27 @@ func CreateConfig() *Config {
 }
 
 func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
-	return &HeaderTransform {
-		headers:  config.Headers,
-		next:   next,
-		name:   name,
+	return &HeaderTransform{
+		headers: config.Headers,
+		next:    next,
+		name:    name,
 	}, nil
 }
 
 func (ht *HeaderTransform) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	log.Print("Request headers - ", req.Header)
+	log.Print("A - ", rw.Header())
 	if req.Header.Get("Origin") != "" {
+		rw.Header().Del("Access-Control-Allow-Origin")
+		rw.Header().Del("Access-Control-Allow-Credentials")
+		rw.Header().Del("Vary")
+
 		rw.Header().Add("Access-Control-Allow-Origin", req.Header.Get("Origin"))
+		rw.Header().Add("Access-Control-Allow-Credentials", "true")
+		rw.Header().Add("Vary", "Origin")
 	}
 
-	rw.Header().Add("Access-Control-Allow-Credentials", "true")
-	rw.Header().Add("Vary", "Origin")
+	log.Print("B - ", rw.Header())
 
 	ht.next.ServeHTTP(rw, req)
 }
